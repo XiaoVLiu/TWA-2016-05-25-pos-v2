192fe77c3619c84e4d2968fa989a98f9b2c8b85d
XiaoVLiu <610834156@qq.com>
2016-05-25 21:17:53 +0800

    complete print

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -87,6 +87,52 @@ function calculateSaveMoney(cartItems) {
     return saveMoney;
 }
 
+function getPromoteItems(cartItems){
+    var promoteItems = [];
+
+    cartItems.forEach(function(item){
+        if (item.freeCount>0){
+            var promoteItem = {};
+            promoteItem.name = item.name;
+            promoteItem.freeCount = item.freeCount;
+            promoteItem.unit = item.unit;
+            promoteItems.push(promoteItem);
+        }
+    });
+    return promoteItems;
+}
+
+function buildReceiptString(cartItems, promoteItems, total, saveMoney){
+    var dateDigitToString = function (num) {
+        return num < 10 ? '0' + num : num;
+    };
+    var currentDate = new Date(),
+        year = dateDigitToString(currentDate.getFullYear()),
+        month = dateDigitToString(currentDate.getMonth() + 1),
+        date = dateDigitToString(currentDate.getDate()),
+        hour = dateDigitToString(currentDate.getHours()),
+        minute = dateDigitToString(currentDate.getMinutes()),
+        second = dateDigitToString(currentDate.getSeconds()),
+        formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+
+    var receiptString = '***<没钱赚商店>购物清单***\n' + '打印时间：' + formattedDateString + '\n' + '----------------------\n';
+    cartItems.forEach(function(item){
+        receiptString += '名称：'+item.name+'，数量：'+item.count+item.unit+'，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.actualSubTotal.toFixed(2)+'(元)\n';
+    });
+    receiptString += "----------------------\n";
+
+    receiptString += '挥泪赠送商品：\n';
+    promoteItems.forEach(function(item){
+        receiptString += '名称：'+item.name+'，数量：'+item.freeCount+item.unit+'\n';
+    });
+
+    receiptString += '----------------------\n';
+    receiptString += '总计：'+total.toFixed(2)+'(元)\n';
+    receiptString += '节省：'+saveMoney.toFixed(2)+'(元)\n';
+    receiptString += "**********************";
+
+    console.log(receiptString);
+}
 function printInventory(inputs) {
     var allItems = loadAllItems();
     var promotions = loadPromotions();
@@ -98,6 +144,7 @@ function printInventory(inputs) {
 
     var total = calculateTotal(cartItems);
     var saveMoney = calculateSaveMoney(cartItems);
+    var promoteItems = getPromoteItems(cartItems);
 
-    console.debug(total, cartItems, saveMoney);
+    buildReceiptString(cartItems, promoteItems, total, saveMoney);
 }
